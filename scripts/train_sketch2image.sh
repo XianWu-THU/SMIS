ea9a2301a43ef6f193a6c23855f2c6b3053c1650
XianWu <thuwx95@gmail.com>
2020-05-05 22:48:52 +0800

    vae

--- scripts/train_sketch2image.sh
+++ scripts/train_sketch2image.sh
@@ -1,1 +1,1 @@
-python train.py --name fashion_gt --dataset_mode sketch --dataroot ./datasets/fashion2/ --no_instance --gpu_ids 1 --ngf 64 --batchSize 2 --use_vae --niter 20 --niter_decay 20  --model smis --netE conv --netG deepfashion
+python train.py --name fashion_gt --dataset_mode sketch --dataroot ./datasets/fashion2/ --no_instance --gpu_ids 1 --ngf 64 --batchSize 2 --niter 20 --niter_decay 20  --model smis --netE conv --netG deepfashion
